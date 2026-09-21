ee9d3e88d7d63087057087cab1829d754bc5a58a
Harold ocana <haroldocana@comptech.lat>
2026-09-20 22:08:29 -0600

    Remove fullscreen console open handler from VrTherapyModule

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -673,7 +673,6 @@ export default function App() {
             <VrTherapyModule
               patient={currentPatient}
               onUpdatePatientVrData={handleUpdatePatientVrData}
-              onOpenFullscreenConsole={() => setIsFullscreenConsoleOpen(true)}
             />
           </div>
         )}
